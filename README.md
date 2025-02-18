3d38dee3ab5f05e52a4649b34415071b10ac4a36
Rampranav <vrampranav9@gmail.com>
2025-02-18 13:10:05 +0530

    create context

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ### Exercise - 1:
 
 - Create 2 components : Cart & ProductListing
+
+### Exercise - 2:
+
+- Create a Context & wrap the components in ContextProvider (cart-context.js) where App is rendered
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { CartContext } from "./cart-context.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
+  <CartContext.Provider value={{count:4}}>
     <App />
-)
+  </CartContext.Provider>
+);
