3abe50c001ae9a7ff43c981d2119e061663d713b
Rampranav <vrampranav9@gmail.com>
2025-02-18 12:37:48 +0530

    add 2 components

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # useContext Examples
+
+### Exercise - 1:
+
+- Create 2 components : Cart & ProductListing
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'
 
+function Cart(){
+  return (
+    <h1>Items in Cart</h1>
+  )
+}
+
+function ProductList(){
+  const products =['1','2','3','4','5'];
+  return (
+    <div>
+      {products.map((item,index)=><p key={index}>Product {item}</p>)}
+    </div>
+  )
+}
+
 function App() {
   return (
     <>
-      <h1>Use Context Example!</h1>
+      <Cart/>
+      <ProductList/>
     </>
   )
 }
